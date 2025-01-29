2336c0927bd8f37223d9c23a213f949d452ea2e4
zeeeing <zjchew@singnet.com.sg>
2025-01-29 17:32:44 +0800

    deleted unnecessary files, updated project page

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -27,7 +27,11 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
       animate={isInView ? { opacity: 1, y: 0 } : {}}
       transition={{ duration: 0.5, delay: project.id * 0.2 }} // use id key to determine the delay of transition
     >
-      <Card isBlurred className="" shadow="sm">
+      <Card
+        isBlurred
+        className="border-none bg-background/60 dark:bg-default-100/50"
+        shadow="sm"
+      >
         <CardBody>
           <div className="grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-4 items-center justify-center">
             <div className="relative col-span-6 md:col-span-4">
@@ -47,7 +51,7 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
                 <div>
                   <div className="flex flex-wrap gap-2">
                     {project.tools.map((tool, index) => (
-                      <Chip key={index} color="secondary" variant="faded">
+                      <Chip key={index} color="secondary" variant="flat">
                         {tool}
                       </Chip>
                     ))}
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,7 +5,7 @@ import { projects } from "../constants/constants";
 
 export default function Projects() {
   return (
-    <div className="pt-36 px-20 flex flex-col w-full font-serif gap-12">
+    <div className="py-36 px-20 flex flex-col w-full font-serif gap-12">
       <h1 className="text-5xl font-bold text-gray-800 text-left">
         My Projects
       </h1>
