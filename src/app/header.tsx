0c25d7a7711cfb29e17c175ae587d5d0cc520d50
zeeeing <zjchew@singnet.com.sg>
2025-01-29 09:37:09 +0800

    added navbar skeleton

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,11 +1,20 @@
+import Link from "next/link";
 import { ThemeSwitcher } from "./components/ThemeSwitcher";
 
 export default function Header() {
   return (
-    <div className="absolute w-full">
-      <header className="flex justify-end p-4">
-        <ThemeSwitcher />
-      </header>
+    <div className="fixed top-0 left-0 w-full z-50 font-serif font-bold">
+      <nav className="flex justify-between items-center p-6">
+        <div className="text-2xl">CZJ</div>
+        <div className="flex gap-12">
+          <Link href={"/"}>Home</Link>
+          <Link href={"/projects"}>Projects</Link>
+          <Link href={"/about"}>About</Link>
+        </div>
+        <div>
+          <ThemeSwitcher />
+        </div>
+      </nav>
     </div>
   );
 }
