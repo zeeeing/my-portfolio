0697076e6e702d73ed259e5b8cafdec068575336
zeeeing <zjchew@singnet.com.sg>
2025-02-06 09:48:36 +0100

    removed unused imports

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { HeroUIProvider } from "@heroui/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
