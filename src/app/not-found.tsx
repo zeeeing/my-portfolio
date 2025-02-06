34c1b944842d9c616d80abd46a5759352149ea67
zeeeing <zjchew@singnet.com.sg>
2025-02-06 07:34:43 +0100

    fixed eslint rules

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,7 +9,7 @@ export default function NotFound() {
       <h1 className="text-9xl font-bold">404</h1>
       <h2 className="mt-4 text-4xl font-bold">Page Not Found</h2>
       <p className="mt-2 text-lg">
-        Oops! The page you're looking for doesn't exist.
+        {`Oops! The page you're looking for doesn't exist.`}
       </p>
       <Button className="mt-6 px-6 py-3" as={Link} href="/" color="primary">
         Go Home
