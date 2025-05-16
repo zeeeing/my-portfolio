5d595faae61ef2d49e919088504d8ad59cfd5c2c
zeeeing <zjchew@singnet.com.sg>
2025-05-16 16:55:40 +0200

    updated WriteNow description

--- src/app/ui/constants.tsx
+++ src/app/ui/constants.tsx
@@ -40,7 +40,7 @@ export const projects = [
     image: "/path/to/image2.jpg",
     title: "WriteNow",
     description:
-      "WriteNow is a task management platform that integrates Google account synchronization with AI-driven workflows. Powered by OpenAI, it streamlines task organisation and meeting management for a more efficient workflow.",
+      "WriteNow is a digital PA web app that streamlines admin workflows—organising tasks, auto-scheduling meetings, generating AI meeting minutes, and turning email summaries into actionable tasks.",
     tools: [
       "ReactJS",
       "TailwindCSS",
